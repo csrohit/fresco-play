2afed76ff727d597c98732fda315c5f2df997672
KvvcK1025 <30663264+KvvcK1025@users.noreply.github.com>
2021-04-14 16:26:56 +0530

    Update HospitalRepository.java

--- Spring Cantabile/full-stack-api/HospitalRepository.java
+++ Spring Cantabile/full-stack-api/HospitalRepository.java
@@ -1,4 +1,4 @@
-package com.csrohit.cantabilerest.solution;
+package com.example.project;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
