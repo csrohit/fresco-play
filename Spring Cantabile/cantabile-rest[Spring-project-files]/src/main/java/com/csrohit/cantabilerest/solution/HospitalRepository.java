2772920200c5df417314a77948e208f698c4e307
KvvcK1025 <30663264+KvvcK1025@users.noreply.github.com>
2021-04-14 16:32:57 +0530

    Delete Spring Cantabile/cantabile-rest[Spring-project-files] directory
    dupilcate folder

--- Spring Cantabile/cantabile-rest[Spring-project-files]/src/main/java/com/csrohit/cantabilerest/solution/HospitalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.csrohit.cantabilerest.solution;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
-
-}
