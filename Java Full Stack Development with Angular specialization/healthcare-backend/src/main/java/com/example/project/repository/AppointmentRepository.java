01757abbd1fa6bf2bf70d1194995a77c0dee6740
Rohit Nimkar <nehalnimkar@gmail.com>
2021-04-17 19:25:12 +0530

    remove project files:rewind:

--- Java Full Stack Development with Angular specialization/healthcare-backend/src/main/java/com/example/project/repository/AppointmentRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import Model.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface AppointmentRepository extends JpaRepository<Appointment,String>{
-
-}
