670f43eae2751351be0f4d833020e6964a5690af
Rohit Nimkar <nehalnimkar@gmail.com>
2021-06-04 09:16:13 +0530

    :white_check_mark:Logging: All tests passed

--- Java Architecture Program/logging_fp/CodeLab-Logging/src/main/java/com/fresco/codelab/logging/RequestAppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.fresco.codelab.logging;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-public class RequestAppConfig{
-}
--- Java Architecture Program/logging_fp/CodeLab-Logging/src/main/resources/static/js/repocode.js
+++
@@ -1,7 +0,0 @@
-function openEditor(element){
-	element.parentElement.getElementsByClassName('code-editor')[0].style.display = "block";
-}
-
-function closeEditor(element){
-	element.parentElement.parentElement.style.display = "none";
-}
