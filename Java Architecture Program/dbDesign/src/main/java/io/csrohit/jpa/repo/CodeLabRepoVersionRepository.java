7c71717af7827b73be7fe8a30a21bc787b083499
Rohit Nimkar <nehalnimkar@gmail.com>
2021-06-04 09:20:49 +0530

    :fire: refactor

--- Java Architecture Program/dbDesign/src/main/java/io/csrohit/jpa/repo/CodeLabRepoVersionRepository.java
+++
@@ -1,9 +0,0 @@
-package io.csrohit.jpa.repo;
-
-import io.csrohit.jpa.model.CodeLabRepoVersion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CodeLabRepoVersionRepository  extends JpaRepository<CodeLabRepoVersion, Integer>{
-}
