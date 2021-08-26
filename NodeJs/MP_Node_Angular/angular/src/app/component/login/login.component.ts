a13e903f615b6e91b2eb1e618690bfa021b2f43c
Rohit Nimkar <nehalnimkar@gmail.com>
2021-08-26 10:34:37 +0530

    :heavy_check_mark:Login: All tests passed

--- NodeJs/MP_Node_Angular/angular/src/app/component/login/login.component.ts
+++ NodeJs/MP_Node_Angular/angular/src/app/component/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,  Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ApiService } from '../../services/api.service';
@@ -7,51 +7,63 @@ import { DataService } from '../../services/data.service';
 import * as alertify from 'alertify.js';//import
 
 @Component({
-	selector: 'app-login',
-	templateUrl: './login.component.html',
-	styleUrls: ['./login.component.css']
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-	
-	isLoggedIn: boolean = false;
-	loginForm: FormGroup;
-	isLoginFailed: boolean = false;
 
-	emptyUserName = 'You must enter a username';
-	minlengthUserName = 'User name must be at least 3 characters long';
-	maxlengthUserName = 'Username cannot exceed 20 characters';
-	userNamePattern = 'Username should be in alphanumeric only';
+  isLoggedIn: boolean = false;
+  loginForm: FormGroup;
+  isLoginFailed: boolean = false;
 
-	emptyPassword = 'You must enter a password';
-	minlengthPassword = 'Password must be at least 8 characters long';
-	maxlengthPassword = 'Password cannot exceed 20 characters';
-	passwordPattern = 'Pattern does not match';
+  emptyUserName = 'You must enter a username';
+  minlengthUserName = 'User name must be at least 3 characters long';
+  maxlengthUserName = 'Username cannot exceed 20 characters';
+  userNamePattern = 'Username should be in alphanumeric only';
 
-	constructor(private route: Router, private dataService: DataService) {
-	 }
+  emptyPassword = 'You must enter a password';
+  minlengthPassword = 'Password must be at least 8 characters long';
+  maxlengthPassword = 'Password cannot exceed 20 characters';
+  passwordPattern = 'Pattern does not match';
 
-	ngOnInit() {
+  constructor(private route: Router, private dataService: DataService) {
+  }
 
-		// add necessary validators
+  ngOnInit() {
 
-		this.loginForm = new FormGroup({
-			userName: new FormControl(''),
-			password: new FormControl('')
-		});
-	}
+    // add necessary validators
 
-	doLogin() {
+    this.loginForm = new FormGroup({
+      userName: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20), Validators.pattern(/^[a-zA-z0-9]*$/)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(20), Validators.pattern(/^[a-zA-Z0-9$]*$/)])
+    });
+  }
 
-		// call authenticateUser method to perform login operation
-		// if success, redirect to profile page
-		// else display appropriate error message
-		   // reset the form
+  doLogin() {
 
-	}
+    // call authenticateUser method to perform login operation
+    // if success, redirect to profile page
+    // else display appropriate error message
+    // reset the form
+    this.dataService.authenticateUser(this.loginForm.value.userName, this.loginForm.value.password)
+      .subscribe(res => {
+        if (res) {
 
-	signUp() {
-		// should navigate to register new user page
-	}
+          this.route.navigate(['profile']);
+        } else {
+
+          localStorage.clear();
+        }
+      });
+
+    this.loginForm.reset();
+  }
+
+  signUp() {
+    // should navigate to register new user page
+    this.route.navigate(['register_user']);
+  }
 
 }
 
