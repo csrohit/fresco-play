34f68e6fa92a858e544d094e3eea4434341f15a3
Rohit Nimkar <nehalnimkar@gmail.com>
2021-11-30 21:24:06 +0530

    :heavy_check_mark: FormComponent: Passed

--- Java Full Stack Development with Angular specialization/MP Java Springboot Angular Full Stack Application/Angular/src/app/component/form/form.component.ts
+++ Java Full Stack Development with Angular specialization/MP Java Springboot Angular Full Stack Application/Angular/src/app/component/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup, FormBuilder,Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { DatePipe } from '@angular/common';
 import { Router } from '@angular/router';
 import { Patient } from '../../models/patient';
@@ -36,23 +36,34 @@ export class FormComponent implements OnInit {
     this.today = this.datePipe.transform(Date.now(), 'yyyy-MM-dd');
   }
 
-  constructor( fb: FormBuilder,private datePipe: DatePipe,private route: Router, private dataService: DataService){
+  constructor(fb: FormBuilder, private datePipe: DatePipe, private route: Router, private dataService: DataService) {
     // add necessary validators
     this.complexForm = fb.group({
-      'name' : [''],
-      'gender' : [null],
-      'dob' : [null],
-      'mobile' : [''],
-      'email' : ['']
+      'name': ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      'gender': [null, Validators.required],
+      'dob': [null, Validators.required],
+      'mobile': ['', [Validators.required, Validators.maxLength(10), Validators.minLength(10), Validators.pattern(/^[0-9]{10,}/)]],
+      'email': ['', [Validators.required, Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)]],
     })
   }
 
-  submitForm(value: any){
+  submitForm(val: any) {
 
     // should reister new patient using service
-       // fields that need to be added: patient_name, patient_gender, patient_dob, patient_mobile, patient_email
+    // fields that need to be added: patient_name, patient_gender, patient_dob, patient_mobile, patient_email
     // if added successfully should redirect to 'patientList' page
 
+    this.dataService.registerPatient({
+      patient_name: val.name,
+      patient_gender: val.gender,
+      patient_dob: val.dob,
+      patient_mobile: val.mobile,
+      patient_email: val.email
+    }).subscribe(res => {
+      this.route.navigate(['patientList'])
+    }, err => {
+
+    })
   }
 
 }
