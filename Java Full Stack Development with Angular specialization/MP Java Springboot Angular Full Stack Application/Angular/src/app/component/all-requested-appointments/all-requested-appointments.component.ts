6d59b18c4290aa36212a0fd0dc01dd22a2d28ccc
Rohit Nimkar <nehalnimkar@gmail.com>
2021-11-30 19:48:01 +0530

    :heavy_check_mark: AllRequestedAppointments:Passed

--- Java Full Stack Development with Angular specialization/MP Java Springboot Angular Full Stack Application/Angular/src/app/component/all-requested-appointments/all-requested-appointments.component.ts
+++ Java Full Stack Development with Angular specialization/MP Java Springboot Angular Full Stack Application/Angular/src/app/component/all-requested-appointments/all-requested-appointments.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Router } from '@angular/router';
+import { Appointment } from 'src/app/models/appointment';
 // import * as alertify from 'alertify.js';
 
 @Component({
@@ -12,29 +13,43 @@ export class AllRequestedAppointmentsComponent implements OnInit {
 
 	allAppointments;
 
-  constructor(private dataService: DataService, private route: Router) { 
+  constructor(private dataService: DataService, private route: Router) {
   }
 
   ngOnInit() {
     // call appointments method by default
+    this.appointments();
   }
 
   appointments() {
 
     // get all requested appointments from service
+    this.dataService
+      .requestedAppointments()
+      .subscribe(appointments =>{
+        this.allAppointments = appointments;
+      }, err =>{
 
+      })
   }
 
   view(patientId) {
 
     // should navigate to 'patientList' page with selected patientId
+    this.route.navigate(['patientList', patientId]);
 
   }
 
   cancelAppointment(id) {
 
     // delete selected appointment uing service
+    this.dataService
+      .deleteAppointment(id)
+      .subscribe(res => {
+        this.appointments();
+      }, err =>{
 
+      });
     // After deleting the appointment, get all requested appointments
 
 
