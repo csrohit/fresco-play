4265d18d3d867b0cf84241a10c040ca39e94fabf
Rohit Nimkar <nehalnimkar@gmail.com>
2021-11-30 10:24:49 +0530

    :heavy_check_mark: APIService: All tests passed

--- Java Full Stack Development with Angular specialization/MP Java Springboot Angular Full Stack Application/Angular/src/app/services/api.service.ts
+++ Java Full Stack Development with Angular specialization/MP Java Springboot Angular Full Stack Application/Angular/src/app/services/api.service.ts
@@ -14,7 +14,7 @@ export class ApiService {
   API_URL: String;
 
   constructor(private http: HttpClient) {
-    
+
     this.API_URL = 'api';
 
   }
@@ -22,81 +22,81 @@ export class ApiService {
   public checkLogin(user_name: string, password: string): Observable<any> {
     // should return response from server
 
-    // handle error 
+    // handle error
 
-    return;
+    return this.http.post(`${this.API_URL}/signin`, {user_name, password});
   }
 
   public regNewUser(regNewUser): Observable<any> {
     // should return response from server
 
-    // handle error 
+    // handle error
 
-    return;
+    return this.http.post(`${this.API_URL}/register`, regNewUser);
   }
 
   public getUserDetails(userId: string): Observable<any> {
     // should return user details retireved from server
 
-    // handle error 
+    // handle error
 
-    return;
+    return this.http.get(`${this.API_URL}/viewprofile/${userId}`);
   }
 
   public updateDetails(userId:string, userDetails: any): Observable<any> {
     // should return response from server
 
-    // handle error 
+    // handle error
 
-    return;
+    return this.http.put(`${this.API_URL}/editprofile/${userId}`, userDetails);
   }
 
   public registerPatient(patient: any): Observable<any> {
     // should return response from server if patientDetails added successfully
 
-    // handle error 
+    // handle error
 
-    return;
+    return this.http.post(`${this.API_URL}/patients/register`, patient);
   }
 
   public getAllPatientsList(): Observable<any> {
     // should return all patients from server
 
-    // handle error 
+    // handle error
 
-    return;
+    return this.http.get('api/patients/list/');
   }
 
   public getParticularPatient(patientId): Observable<any> {
     // should return particular patient details from server
 
-    // handle error 
+    // handle error
 
-    return;
+    return this.http.get(`${this.API_URL}/patients/view/${patientId}`);
   }
 
   public diseasesList(): Observable<any> {
     // should return diseases from server
 
-    // handle error 
+    // handle error
 
-    return;
+    return this.http.get(`${this.API_URL}/diseases`);
   }
 
   public scheduleAppointment(appointmentDetails: any) : Observable<any>{
     // should return response from server if appointment booked successfully
 
-    // handle error 
+    // handle error
 
-    return;
+    return this.http.post(`${this.API_URL}/appointment/register`, appointmentDetails);
   }
 
   public getSinglePatientAppointments(patientId): Observable<any> {
     // should return appointments of particular patient from server
 
-    // handle error 
+    // handle error
 
-    return;
+    return this.http.get(`${this.API_URL}/appointment/list/${patientId}`);
   }
 
   public deleteAppointment(appointmentId): Observable<any>  {
@@ -104,15 +104,15 @@ export class ApiService {
 
     // handle error
 
-    return;
+    return this.http.delete(`${this.API_URL}/appointment/delete/${appointmentId}`);
   }
 
   public requestedAppointments(): Observable<any> {
     // should return all requested appointments from server
 
-    // handle error 
+    // handle error
 
-    return;
+    return this.http.get(`${this.API_URL}/appointment/list`);
   }
 
 
