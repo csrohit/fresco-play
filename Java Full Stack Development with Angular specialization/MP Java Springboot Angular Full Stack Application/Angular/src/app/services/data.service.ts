91df11a51205de27f1d46bf704ac28ca4290551d
Rohit Nimkar <nehalnimkar@gmail.com>
2021-11-30 09:45:11 +0530

    :heavy_check_mark: DataService all tests passed

--- Java Full Stack Development with Angular specialization/MP Java Springboot Angular Full Stack Application/Angular/src/app/services/data.service.ts
+++ Java Full Stack Development with Angular specialization/MP Java Springboot Angular Full Stack Application/Angular/src/app/services/data.service.ts
@@ -8,7 +8,7 @@ import { Patient } from '../models/patient';
 import { Appointment } from '../models/appointment';
 
 import { ApiService } from './api.service';
-import { tap } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class DataService {
@@ -28,89 +28,108 @@ export class DataService {
 
     // return true if user authenticated
 
-    // return false if user not authenticated 
+    // return false if user not authenticated
 
-    return;
+    return this.api.checkLogin(user_name, password)
+      .pipe(
+        map(user => {
+          if (user.success) {
+            this.userId = user.id;
+            localStorage.setItem('id', user.id)
+            localStorage.setItem('token', user.token)
+            return true;
+          } else {
+            return false;
+          }
+        }),
+        catchError(err => {
+        this.userId = null;
+        localStorage.clear();
+        return of(false);
+      }))
+      ;
   }
 
   getAuthStatus(): Observable<boolean> {
     // return true/false as a auth status
 
-    return
+    return of(this.userId ? true: false);
   }
 
   regNewUser(regNewUser): Observable<any> {
     // should return response retrieved from ApiService
 
-    // handle error 
+    // handle error
 
-    return;
+    return this.api.regNewUser(regNewUser);
   }
 
   doLogOut() {
     // You should remove the key 'id', 'token' if exists
+    localStorage.clear();
+    this.userId = null;
   }
 
-  getUserDetails(): Observable<any> {
+  getUserDetails(userId = this.userId): Observable<any> {
     // should return user details retrieved from api service
 
-    return;
+    return this.api.getUserDetails(userId);
   }
 
-  updateProfile(userId:string, userDetails): Observable<boolean> {
+  updateProfile(userId:string, userDetails: any): Observable<any> {
     // should return response retrieved from ApiService
 
-    // handle error 
+    // handle error
 
-    return;
+    return this.api.updateDetails(userId, userDetails);
   }
 
-  registerPatient(patientDetails): Observable<any> {
+  registerPatient(patientDetails: any): Observable<any> {
     // should return response retrieved from ApiService
 
-    // handle error 
+    // handle error
 
-    return;
+    return this.api.registerPatient(patientDetails);
   }
 
-  getAllPatientsList(): Observable<any> {
+  getAllPatientsList(): Observable<any[]> {
     // should return all patients from server
 
-    // handle error 
+    // handle error
 
-    return;
+    return this.api.getAllPatientsList();
   }
 
-  getParticularPatient(id): Observable<any> {
+  getParticularPatient(id: Patient['patient_Id']): Observable<any> {
     // should return particular patient details from server
 
-    // handle error 
+    // handle error
 
-    return;
+    return this.api.getParticularPatient(id);
   }
-  
+
   diseasesList(): Observable<any> {
     // should return diseases from server
 
-    // handle error 
+    // handle error
 
-    return;
+    return this.api.diseasesList();
   }
 
   scheduleAppointment(appointmentDetails): Observable<any> {
     // should return response from server if appointment booked successfully
 
-    // handle error 
+    // handle error
 
-    return;
+    return this.api.scheduleAppointment(appointmentDetails);
   }
 
   getSinglePatientAppointments(patientId): Observable<any> {
     // should return appointments of particular patient from server
 
-    // handle error 
+    // handle error
 
-    return;
+    return this.api.getSinglePatientAppointments(patientId);
   }
 
   deleteAppointment(appointmentId): Observable<any> {
@@ -118,15 +137,15 @@ export class DataService {
 
     // handle error
 
-    return
+    return this.api.deleteAppointment(appointmentId);
   }
 
   requestedAppointments(): Observable<any> {
     // should return all requested appointments from server
 
-    // handle error 
+    // handle error
 
-    return;
+    return this.api.requestedAppointments();
   }
 
   private handleError(error: HttpErrorResponse) {
