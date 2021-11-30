683aa1c7f81b1b6cea09cf52a901152bc6098138
Rohit Nimkar <nehalnimkar@gmail.com>
2021-11-30 19:39:50 +0530

    :heavy_check_mark: ProfileComponent: Passed

--- Java Full Stack Development with Angular specialization/MP Java Springboot Angular Full Stack Application/Angular/src/app/component/profile/profile.component.ts
+++ Java Full Stack Development with Angular specialization/MP Java Springboot Angular Full Stack Application/Angular/src/app/component/profile/profile.component.ts
@@ -22,7 +22,7 @@ export class ProfileComponent implements OnInit {
   mobileErrMsg = 'You must enter a valid mobile number';
   emailErrMsg = 'You must enter a valid Email ID';
   locationErrMsg = 'You must enter the location';
-  constructor(private dataService: DataService) { 
+  constructor(private dataService: DataService) {
 
   }
 
@@ -32,38 +32,60 @@ export class ProfileComponent implements OnInit {
     // username should be disabled. it should not be edited
 
     this.editProfileForm = new FormGroup({
-      userName: new FormControl(''),
-      mobile: new FormControl(''),
-      email: new FormControl(''),
-      location: new FormControl('')
+      userName: new FormControl('', Validators.required),
+      mobile: new FormControl('', [Validators.required, Validators.pattern(/^[0-9]{10}$/)]),
+      email: new FormControl('', [Validators.required, Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)]),
+      location: new FormControl('', Validators.required)
     });
-    
+
+    this.editProfileForm.get('userName').disable();
     // get profile details and display it
-    
+    this.getProfileDetails();
+
   }
 
   getProfileDetails() {
 
     // retrieve user details from service using userId
+    this.dataService.getUserDetails()
+    .subscribe(res => {
+      this.userDetails = res;
+    }, err => {
+
+    });
 
   }
 
   changeMyProfile() {
 
     // if successfully changed the profile it should display new details hiding the form
+    this.dataService.updateProfile(this.userDetails.id, this.editProfileForm.value)
+    .subscribe(res => {
+      if(res) {
+        this.discardEdit();
+      this.getProfileDetails();
+      }
+    }, err => {
 
+    })
   }
 
   editMyProfile() {
 
     // change editProfile property value appropriately
-
+    this.editProfile = true;
+    this.editProfileForm.patchValue({
+      userName: this.userDetails.user_name,
+      email: this.userDetails.user_email,
+      mobile: this.userDetails.user_mobile,
+      location: this.userDetails.location
+    })
   }
 
   discardEdit() {
 
     // change editProfile property value appropriately
-
+    this.editProfile = false;
   }
 
 }
