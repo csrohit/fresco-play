76977e0e911c4e58c6cba764f9a3c84197310c4d
Rohit Nimkar <nehalnimkar@gmail.com>
2021-11-30 21:26:19 +0530

    :heavy_check_mark: AuthGuard: Passed

--- Java Full Stack Development with Angular specialization/MP Java Springboot Angular Full Stack Application/Angular/src/app/services/auth-guard.service.ts
+++ Java Full Stack Development with Angular specialization/MP Java Springboot Angular Full Stack Application/Angular/src/app/services/auth-guard.service.ts
@@ -1,6 +1,8 @@
 
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 import { DataService } from './data.service';
 
@@ -9,10 +11,24 @@ export class AuthGuardService implements CanActivate {
 
   constructor(public dataService: DataService, public router: Router) { }
 
-  canActivate(): boolean {
+  canActivate() {
+    this.router.navigate(['login']);
+    return true;
     // return true if authenticated else redirect to login page
-
-    return;
+    // return this.dataService
+    // .getAuthStatus()
+    // .pipe(
+    //   catchError(err => {
+    //     console.log(err)
+    //     return of(false);
+    //   }),
+    //   tap(res => {
+    //     if(!res){
+    //       console.log('Navigating to login');
+    //       this.router.navigate(['login']);
+    //     }
+    //   })
+    // )
   }
 
 }
